1c65d7ef9e086c5bc53da27cf1fae96eb46d358b
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-09 16:48:24 +0100

    base setup

---
+++ app.js
@@ -0,0 +1,30 @@
+require('dotenv').config();
+
+require('./models/connection');
+
+var express = require('express');
+var path = require('path');
+var cookieParser = require('cookie-parser');
+var logger = require('morgan');
+
+var indexRouter = require('./routes/index');
+var usersRouter = require('./routes/users');
+
+var app = express();
+
+const cors = require('cors');
+app.use(cors());
+
+const fileUpload = require('express-fileupload');
+app.use(fileUpload());
+
+app.use(logger('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
+
+module.exports = app;
