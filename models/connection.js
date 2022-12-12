11e3ce64427e286770ece977d1bf47883d6e1861
yiengo <yieng.hu@gmail.com>
2022-12-12 14:40:37 +0100

    base de données users

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = process.env.CONNECTION_STRING;
+const connectionString = "mongodb+srv://Karim:LaCapsule@cluster0.zlaetun.mongodb.net/crowdfurring";
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
     .then(() => console.log('Database connected'))
---
+++ models/users.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const paymentMethodSchema = mongoose.Schema({
+paymentName: String,
+number: Number,
+expirationCode: Date,
+securityCode: Number,
+nameOnCard: String,
+});
+const addressSchema = mongoose.Schema({
+street: String,
+additionnal: String,
+zipCode: Number,
+city: String,
+});
+
+const userSchema = mongoose.Schema({
+  firstname: String,
+  lastname: String,
+  email: String,
+  password: String,
+  address: addressSchema,
+  paymentMethods: [paymentMethodSchema],
+  token: String,
+  picture: String,
+  association: { type: mongoose.Schema.Types.ObjectId, ref: 'associations' },
+  admin: Boolean,
+  isConfirmed: Boolean,
+  phoneNumber: Number,
+  IBAN: String,
+
+});
+
+const User = mongoose.model('users', userSchema);
+
+module.exports = User;
