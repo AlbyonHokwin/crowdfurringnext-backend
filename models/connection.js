421aa6eba03da5ab66f996c8d4799cc510f8ff3d
yiengo <yieng.hu@gmail.com>
2022-12-12 15:07:10 +0100

    base de données users2

--- models/connection.js
+++ models/connection.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-const connectionString = "mongodb+srv://Karim:LaCapsule@cluster0.zlaetun.mongodb.net/crowdfurring";
+const connectionString = process.env.CONNECTION_STRING;
 
 mongoose.connect(connectionString, { connectTimeoutMS: 2000 })
     .then(() => console.log('Database connected'))
