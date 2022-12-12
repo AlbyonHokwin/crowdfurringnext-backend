e50130076aa48ae16147bce42eb60a77d893a6ae
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-12 15:42:36 +0100

    add get route to get specified slug

--- routes/pots.js
+++ routes/pots.js
@@ -2,6 +2,12 @@ const express = require('express');
 const router = express.Router();
 const Pot = require('../models/pots');
 
+router.get('/:slug', async (req, res) => {
+    const pot = await Pot.findOne({ slug: req.params.slug });
 
+    pot ?
+        res.json({ result: true, pot }) :
+        res.json({ result: false, error: 'No pots found' })
+})
 
 module.exports = router;
