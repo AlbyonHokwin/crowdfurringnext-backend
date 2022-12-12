057c308b3cc096503facfd247bcabf9edc00b9bc
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-12 14:17:02 +0100

    creation of pots route

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var potsRouter = require('./routes/pots');
 
 var app = express();
 
@@ -26,5 +27,6 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/pots', potsRouter);
 
 module.exports = app;
---
+++ routes/pots.js
@@ -0,0 +1,7 @@
+const express = require('express');
+const router = express.Router();
+const Pot = require('../models/pots');
+
+
+
+module.exports = router;
