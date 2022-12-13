4917b3aa8b3fed954e36ec8e1c23cf36ef391955
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-13 09:46:27 +0100

    modify pots route by adding route to get all validated pots

--- routes/pots.js
+++ routes/pots.js
@@ -2,13 +2,26 @@ const express = require('express');
 const router = express.Router();
 const Pot = require('../models/pots');
 
-router.get('/:slug', async (req, res) => {
-    const foundPot = await Pot.findOne({ slug: req.params.slug }).populate('user');
+// Route 'GET'
+// Allow to get the pot with corresponding slug parameter
+router.get('/slug/:slug', async (req, res) => {
+    const foundPot = await Pot.findOne({ slug: req.params.slug });
 
     if (foundPot) {
         const pot = await foundPot.populate('user');
         res.json({ result: true, pot })
     } else res.json({ result: false, error: 'No pots found' })
+});
+
+// Route 'GET'
+// To get all validated pots
+router.get('/all', async (req, res) => {
+    // const foundPots = await Pot.find({ isValidate: true });
+    const pots = await Pot.find({ isValidate: true }).populate('user');
+
+    if (pots) {
+        res.json({ result: true, length: pots.length, pots });
+    } else res.json({ result: false, error: 'No validated pots' });
 })
 
 module.exports = router;
