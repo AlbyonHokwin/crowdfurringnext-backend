0d80e848bee3ce2d4d6f6258ab35ac6b15bb9091
Camille HAUSTANT <camillehaustant@gmail.com>
2022-12-15 12:23:11 +0100

    remove unnecessary require

--- routes/users.js
+++ routes/users.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-require('../models/connection');
 const User = require('../models/users');
 const { checkBody } = require('../modules/checkBody');
 const bcrypt = require('bcrypt');
