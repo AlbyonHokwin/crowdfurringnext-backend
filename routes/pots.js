393b30cd1d15ecfc4bbc39efbbbeb9247cc21057
Jeremy <marcillaud.jeremy@gmail.com>
2022-12-15 17:27:54 +0100

    connection back and db

--- routes/pots.js
+++ routes/pots.js
@@ -1,26 +1,114 @@
-const express = require('express');
+const express = require("express");
 const router = express.Router();
-const Pot = require('../models/pots');
+const Pot = require("../models/pots");
+const User = require("../models/users");
+require("../models/connection");
+const uniqid = require("uniqid");
+
+const cloudinary = require("cloudinary").v2;
+const fs = require("fs");
 
 // Route 'GET'
 // Allow to get the pot with corresponding slug parameter
-router.get('/slug/:slug', async (req, res) => {
-    const pot = await Pot.findOne({ slug: req.params.slug }).populate('user');
+router.get("/slug/:slug", async (req, res) => {
+  const pot = await Pot.findOne({ slug: req.params.slug }).populate("user");
 
-    if (pot) {
-        res.json({ result: true, pot })
-    } else res.json({ result: false, error: 'No pots found' })
+  if (pot) {
+    res.json({ result: true, pot });
+  } else res.json({ result: false, error: "No pots found" });
 });
 
 // Route 'GET'
 // To get all validated pots
-router.get('/all', async (req, res) => {
-    // const foundPots = await Pot.find({ isValidate: true });
-    const pots = await Pot.find({ isValidate: true }).populate('user');
+router.get("/all", async (req, res) => {
+  // const foundPots = await Pot.find({ isValidate: true });
+  const pots = await Pot.find({ isValidate: true }).populate("user");
 
-    if (pots) {
-        res.json({ result: true, length: pots.length, pots });
-    } else res.json({ result: false, error: 'No validated pots' });
-})
+  if (pots) {
+    res.json({ result: true, length: pots.length, pots });
+  } else res.json({ result: false, error: "No validated pots" });
+});
+
+router.post("/create", async (req, res) => {
+  console.log(req.files);
+  let {
+    animalName,
+    infos,
+    description,
+    compensation,
+    amount,
+    urgent,
+    explanation,
+    token,
+  } = req.body;
+
+  infos = JSON.parse(infos);
+
+  const documents = [req.files.documents].flat();
+  const images = [req.files.images].flat();
+
+  const foundUser = await User.findOne({ token });
+
+  let pictures = [];
+  let files = [];
+  // tant que je n'ai pas le token je passe avec !foundUser
+  if (!foundUser) {
+    for (let image of images) {
+      const imagesPath = `./tmp/${uniqid()}.jpg`;
+      const resultMove = await image.mv(imagesPath);
+
+      if (!resultMove) {
+        const resultCloudinary = await cloudinary.uploader.upload(imagesPath);
+        pictures.push(resultCloudinary.secure_url);
+      } else {
+        res.json({ result: false, error: resultMove });
+      }
+      fs.unlinkSync(imagesPath);
+    }
+
+    for (let document of documents) {
+      const filesPath = `./tmp/${uniqid()}.jpg`;
+      const resultMove = await document.mv(filesPath);
+
+      if (!resultMove) {
+        const resultCloudinary = await cloudinary.uploader.upload(filesPath);
+        files.push({ name: document.name, url: resultCloudinary.secure_url });
+      } else {
+        res.json({ result: false, error: resultMove });
+      }
+      fs.unlinkSync(filesPath);
+    }
+
+    const newPot = new Pot({
+      //   user: data.id
+      contributors: [],
+      animalName,
+      targetAmount: amount,
+      currentAmount: 0,
+      pictures: pictures,
+      description,
+      info: infos,
+      compensation,
+      socialNetworks: [],
+      documents: files,
+      isValidate: false,
+      isClosed: false,
+      urgent,
+      urgenceDescription: explanation,
+      draft: false,
+      startDate: "",
+      endDate: "",
+      duration: "",
+      slug: "",
+    });
+    newPot.save().then((pot) => {
+      if (pot !== null) {
+        return res.json({ result: true });
+      } else {
+        return res.json({ result: false });
+      }
+    });
+  }
+});
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,67 +1,80 @@
-var express = require('express');
+var express = require("express");
 var router = express.Router();
 
-require('../models/connection');
-const User = require('../models/users');
-const { checkBody } = require('../modules/checkBody');
-const bcrypt = require('bcrypt');
-const uid2 = require('uid2');
+require("../models/connection");
+const User = require("../models/users");
+const { checkBody } = require("../modules/checkBody");
+const bcrypt = require("bcrypt");
+const uid2 = require("uid2");
 
-router.post('/signup', (req, res) => {
-  if (!checkBody(req.body, ['email', 'password', 'lastname', 'firstname', 'street', 'zipCode', 'city'])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
+router.post("/signup", (req, res) => {
+  if (
+    !checkBody(req.body, [
+      "email",
+      "password",
+      "lastname",
+      "firstname",
+      "street",
+      "zipCode",
+      "city",
+    ])
+  ) {
+    res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
 
   // Check if the user has not already been registered
-  User.findOne({ email: { $regex: new RegExp(req.body.email, 'i') } }).then(data => {
-    if (data === null) {
-      const hash = bcrypt.hashSync(req.body.password, 10);
-      const newUser = new User({
-        email: req.body.email,
-        password: hash,
-        token: uid2(32),
-        firstname: req.body.firstname,
-        lastname: req.body.lastname,
-        address: {
-          street: req.body.street,
-          additionnal: req.body.additionnal || "",
-          zipCode: req.body.zipCode,
-          city: req.body.city,
-        },
-        paymentMethods: [],
-        picture: "",
-        association: null,
-        admin: false,
-        isConfirmed: false,
-        phoneNumber: "",
-        IBAN: "",        
-      });
+  User.findOne({ email: { $regex: new RegExp(req.body.email, "i") } }).then(
+    (data) => {
+      if (data === null) {
+        const hash = bcrypt.hashSync(req.body.password, 10);
+        const newUser = new User({
+          email: req.body.email,
+          password: hash,
+          token: uid2(32),
+          firstname: req.body.firstname,
+          lastname: req.body.lastname,
+          address: {
+            street: req.body.street,
+            additionnal: req.body.additionnal || "",
+            zipCode: req.body.zipCode,
+            city: req.body.city,
+          },
+          paymentMethods: [],
+          picture: "",
+          association: null,
+          admin: false,
+          isConfirmed: false,
+          phoneNumber: "",
+          IBAN: "",
+        });
 
-      newUser.save().then(newDoc => {
-        res.json({ result: true, token: newDoc.token });
-      });
-    } else {
-      // User already exists in database
-      res.json({ result: false, error: 'User already exists' });
+        newUser.save().then((newDoc) => {
+          res.json({ result: true, token: newDoc.token });
+        });
+      } else {
+        // User already exists in database
+        res.json({ result: false, error: "User already exists" });
+      }
     }
-  });
+  );
 });
 
-router.post('/signin', (req, res) => {
-  if (!checkBody(req.body, ['email', 'password'])) {
-    res.json({ result: false, error: 'Missing or empty fields' });
+router.post("/signin", (req, res) => {
+  if (!checkBody(req.body, ["email", "password"])) {
+    res.json({ result: false, error: "Missing or empty fields" });
     return;
   }
 
-  User.findOne({ email: { $regex: new RegExp(req.body.email, 'i') } }).then(data => {
-    if (bcrypt.compareSync(req.body.password, data.password)) {
-      res.json({ result: true, token: data.token, email: data.email });
-    } else {
-      res.json({ result: false, error: 'User not found or wrong password' });
+  User.findOne({ email: { $regex: new RegExp(req.body.email, "i") } }).then(
+    (data) => {
+      if (bcrypt.compareSync(req.body.password, data.password)) {
+        res.json({ result: true, token: data.token, email: data.email });
+      } else {
+        res.json({ result: false, error: "User not found or wrong password" });
+      }
     }
-  });
+  );
 });
-
 
 module.exports = router;
